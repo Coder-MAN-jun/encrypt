cc526ed3cb43521140ebb0b4f922598248a9c9c8
murtazali.rasulow <murtazali.rasulow.05@gamil.com>
2022-10-15 14:18:55 +0300

    add a new encrypt method sha2

--- encrypt.rb
+++ encrypt.rb
@@ -11,15 +11,15 @@ require 'digest'
 puts 'Введите слово или фразу для шифрования:'
 string = STDIN.gets.chomp
 
-puts 'Каким способом зашифровать'
+puts 'Каким способом зашифровать:'
 puts '1. MD5'
 puts '2. SHA1'
 puts '3. SHA2'
 encrypt_method = STDIN.gets.to_i
 
 until encrypt_method.between?(1, 3)
-	puts 'Выберите 1 или 2'
-	encrypt_method = STDIN.gets.to_i
+  puts 'Выберите 1 или 2'
+  encrypt_method = STDIN.gets.to_i
 end
 
 puts 'Вот что получилось:'
@@ -27,5 +27,5 @@ puts 'Вот что получилось:'
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
-when 3 then puts Digest::SHA2.hexdigest(string) 
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
