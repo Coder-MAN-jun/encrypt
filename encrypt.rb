a3f79de00f35c608b0e6ef6e0ed49955dd7b846c
murtazali.rasulow <murtazali.rasulow.05@gamil.com>
2022-10-15 14:03:08 +0300

    encrypt wit md5 and sha1

---
+++ encrypt.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+# 
+# Программа для шифрования слова с помощью механизма MD5, SHA1 или  SHA2
+# 
+# (c) goodprogrammer.ru
+# 
+# ---
+
+require 'digest'
+
+puts 'Введите слово или фразу для шифрования:'
+string = STDIN.gets.chomp
+
+puts 'Каким способом зашифровать'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
+encrypt_method = STDIN.gets.to_i
+
+until encrypt_method.between?(1, 3)
+	puts 'Выберите 1 или 2'
+	encrypt_method = STDIN.gets.to_i
+end
+
+puts 'Вот что получилось:'
+
+case encrypt_method
+when 1 then puts Digest::MD5.hexdigest(string)
+when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string) 
+end
