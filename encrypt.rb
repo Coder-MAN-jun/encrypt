cc525c1fb44e81c6e0eeca8ce5d279da0834c5eb
murtazali.rasulow <murtazali.rasulow.05@gamil.com>
2022-10-15 14:40:51 +0300

    Deleted a new method sha2

--- encrypt.rb
+++ encrypt.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 # 
-# Программа для шифрования слова с помощью механизма MD5, SHA1 или  SHA2
 # 
 # (c) goodprogrammer.ru
 # 
@@ -14,10 +13,9 @@ string = STDIN.gets.chomp
 puts 'Каким способом зашифровать:'
 puts '1. MD5'
 puts '2. SHA1'
-puts '3. SHA2'
 encrypt_method = STDIN.gets.to_i
 
-until encrypt_method.between?(1, 3)
+until encrypt_method.between?(1, 2)
   puts 'Выберите 1 или 2'
   encrypt_method = STDIN.gets.to_i
 end
@@ -27,5 +25,4 @@ puts 'Вот что получилось:'
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
-when 3 then puts Digest::SHA2.hexdigest(string)
 end
